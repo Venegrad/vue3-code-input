95a92d6c7f54c10aa1c54be277b73d0b78475294
venegrad <venegrad@gmail.com>
2024-06-18 10:17:07 +0300

    fixes

--- src/venCodeInput.vue
+++ src/venCodeInput.vue
@@ -2,11 +2,13 @@
 .code-field(:class="{'error': innerError}")
   .code-field__list
     .code-field__item(v-for="(item, ind) in [...Array(length).keys()]" :ind="'ss'+ind")
-      input.code-field__input(type="text" v-model="innerValue[ind]" @input="goChange($event, innerValue[ind], ind)"  @click="innerError = null" @paste="pasteEvent" @keydown.backspace="backEvent($event, ind)"  ref="codeInput")
+      input.code-field__input(:type="numbersOnly ? 'number' : 'text'" min="0" :inputmode="numbersOnly ? 'numeric' : null" :pattern="numbersOnly ? '[0-9]*' : '.*'" v-model="innerValue[ind]" @input="goChange($event, innerValue[ind], ind)"  @click="innerError = null" @paste="pasteEvent" @keydown.backspace="backEvent($event, ind)"  ref="codeInput")
   .code-field__error(v-if="innerError") {{ innerError }} 
 </template>
 
 <script>
+const regex = RegExp(/[0-9]+/g);
+
 export default {
 	emits: ['update:modelValue', 'changed'],
 	
@@ -47,20 +49,27 @@ export default {
 			if (!val) return null;
 			val = val.toString();
 			val = val.replace(new RegExp(this.disallow, 'g'), '');
+			if(this.numbersOnly) val = val.replace(new RegExp(/[^0-9]/g, 'g'), ''); 
 			return this.upper ? val.toUpperCase() : val.toLowerCase();
 		},
 		backEvent(e, ind) {
 			if(!e.target.value && this.$refs.codeInput[ind - 1]) this.$refs.codeInput[ind - 1].focus();
 		},
 		pasteEvent(e) {
+			
 			e.preventDefault()
-			const copiedData = e.clipboardData.getData('text');
+			let copiedData = e.clipboardData.getData('text');
 			const compiledArray = this.filterStr(copiedData).split('').slice(0,  this.length)
 			this.innerValue = compiledArray
 			if(this.$refs.codeInput[this.innerValue.length - 1]) this.$refs.codeInput[this.innerValue.length - 1].focus()
 		},
 
 		goChange(e, val, ind) { 
+
+			
+			if(this.numbersOnly && !regex.test(e.target.value)) e.target.value = null
+
+
 			this.innerError = null
 			if(!val) return
 			const vs = val.toString();
@@ -82,6 +91,10 @@ export default {
 			default: () => {
 				return /[^a-zA-Z0-9]/g
 			},
+		},
+		numbersOnly: {
+			type: Boolean,
+			default: true
 		},
 		upper: {
 			type: Boolean,
